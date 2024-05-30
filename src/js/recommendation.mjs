fd4653678fddf0ec10ec7a6a29303281dfbcde47
ndongaloris <ndongajared@gmail.com>
2024-05-30 20:05:30 +0200

    pop up window done

--- src/js/recommendation.mjs
+++ src/js/recommendation.mjs
@@ -1,44 +1,5 @@
 import externalServices from "./externalServices.mjs";
 
-function recommendationTemplate(anime){
-    return`<div class="container">
-                <div class="anime-card">
-                    <img src="${anime.image}" alt="">
-                    <h3 class="title">${anime.title}</h3>
-                    <a href="#" id="moreInfo">More Info</a>
-                </div>
-                <div class="pop-up">
-                    <img src="${anime.image}" alt="">
-                    <h2 class="title">${anime.title}</h2>
-                    <p>Ranking: ${anime.ranking}</p>
-                    <p>Genre: ${anime.genres}</p>
-                    <p>Type: ${anime.type}</p>
-                    <p>Episodes: ${anime.episodes}</p>
-                    <p class="title">Status: ${anime.status}</p>
-                    <p>Synopsis: ${anime.synopsis}</p>
-                </div>
-            </div>`
-}
-
-function pageManipulation(pageNumber){
-    return `<a href="#recommendationSection" id="prev">Prev</a>
-            <label for="">Page:<input type="number" Readonly value="${pageNumber}"main="1" max="100"> of 100</label>
-            
-            <a href="#recommendationSection" id="next">Next</a>
-                `
-}
-
-function popUpWindow(){
-    const moreInfo = document.querySelectorAll("#moreInfo");
-    moreInfo.forEach(button =>{
-        button.addEventListener("click", () =>{
-            document.querySelector("body").classList.toggle(".active");
-            document.querySelector("#pop-pup").classList.toggle("active")
-        })
-
-    })    
-}
-
 
 export async function recommend(link){
     const recommendation = new externalServices(link);
@@ -49,7 +10,66 @@ export async function recommend(link){
     let htmlString = data.map(recommendationTemplate);
     
     recommendationSection.insertAdjacentHTML("afterbegin", htmlString.join(""));
+
+    popUpWindow();
     
+}
+
+export function popUpWindow(){
+    const moreInfo = document.querySelectorAll(".moreInfo");   
+    moreInfo.forEach(button => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            // Find the card element
+            const card = event.target.parentElement;
+
+            // Find the sibling pop-up element
+            const popUp = card.nextElementSibling;
+
+            // Toggle the class for the pop-up
+            popUp.classList.toggle("active");
+
+            document.querySelectorAll(".closeWindow").forEach(element => {
+                element.addEventListener("click", (event)=>{
+                    event.preventDefault();
+
+                    const parent = event.target.parentElement;
+                    parent.classList.remove("active");
+            });
+                
+            })
+        });
+    });
+}
+
+function recommendationTemplate(anime){
+    return`<div class="container">
+                <div class="anime-card">
+                    <img src="${anime.image}" alt="">
+                    <h3 class="title">${anime.title}</h3>
+                    <a href="#" class="moreInfo">More Info</a>
+                </div>
+                <div class="pop-up">
+                    <img src="${anime.image}" alt="">
+                    <div class="windowInfo">
+                        <h2 class="title">${anime.title}</h2>
+                        <p>Ranking: ${anime.ranking}</p>
+                        <p>Genre: ${anime.genres}</p>
+                        <p>Type: ${anime.type}</p>
+                        <p>Episodes: ${anime.episodes}</p>
+                        <p class="title">Status: ${anime.status}</p>
+                    </div>
+                    <p>Synopsis: ${anime.synopsis}</p>
+                    <a href="#" class="closeWindow">close</a>
+                </div>
+            </div>`
+}
+
+function pageManipulation(pageNumber){
+    return `<a href="#recommendationSection" id="prev">Prev</a>
+            <label for="">Page:<input type="number" Readonly value="${pageNumber}"main="1" max="100"> of 100</label>
+            
+            <a href="#recommendationSection" id="next">Next</a>`
 }
 
 export default class recommendation{
@@ -92,3 +112,4 @@ export default class recommendation{
         })
     }
 }
+
